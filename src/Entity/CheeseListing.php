60eb57448ff703b2e6b81787369f1d7f3707180b
Robin Charlet <robin.charlet@laposte.net>
2020-08-11 10:38:18 +0200

    Change serialized name + set title with constructor

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -7,6 +7,7 @@ use App\Repository\CheeseListingRepository;
 use Carbon\Carbon;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 
 /**
  * @ApiResource(
@@ -61,8 +62,9 @@ class CheeseListing
      */
     private $isPublished = false;
 
-    public function __construct()
+    public function __construct(string $title = null)
     {
+        $this->title = $title;
         $this->createdAt = new \DateTimeImmutable();
     }
 
@@ -74,13 +76,6 @@ class CheeseListing
     public function getTitle(): ?string
     {
         return $this->title;
-    }
-
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
-
-        return $this;
     }
 
     public function getDescription(): ?string
@@ -98,6 +93,7 @@ class CheeseListing
      * The description of the cheese as raw text.
      *
      * @Groups({"cheese_listing:write"})
+     * @SerializedName("description")
      */
     public function setTextDescription(string $description): self
     {

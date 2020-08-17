53b332088de9a3f9bfe91fb707595ba3fa4e198a
Robin Charlet <robin.charlet@laposte.net>
2020-08-17 11:27:20 +0200

    Passing API user data to Vue on page load

--- src/Controller/FrontendController.php
+++ src/Controller/FrontendController.php
@@ -4,14 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class FrontendController extends AbstractController
 {
     /**
      * @Route("/")
      */
-    public function homepage()
+    public function homepage(SerializerInterface $serializer)
     {
-        return $this->render('frontend/homepage.html.twig');
+        return $this->render('frontend/homepage.html.twig', [
+            'user' => $serializer->serialize($this->getUser(), 'jsonld')
+        ]);
     }
 }

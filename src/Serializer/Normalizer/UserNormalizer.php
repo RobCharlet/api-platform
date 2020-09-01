d002625c8b6354e9875cc5377db4fd7dd2bb5ce5
Robin Charlet <robin.charlet@laposte.net>
2020-09-01 12:44:34 +0200

    Create custom User normalizer to add an owner:read context on the objet. userIsOwner to fix

---
+++ src/Serializer/Normalizer/UserNormalizer.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Serializer\Normalizer;
+
+use App\Entity\User;
+use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+
+class UserNormalizer implements ContextAwareNormalizerInterface, CacheableSupportsMethodInterface, NormalizerAwareInterface
+{
+    use NormalizerAwareTrait;
+
+    private const ALREADY_CALLED = 'USER_NORMALIZER_ALREADY_CALLED';
+
+    /**
+     * @param User $object
+     */
+    public function normalize($object, string $format = null, array $context = array()): array
+    {
+        if ($this->userIsOwner($object)) {
+            $context['groups'][] = 'owner:read';
+        }
+
+        // Prevent infinite loop
+        $context[self::ALREADY_CALLED] = true;
+
+        $data = $this->normalizer->normalize($object, $format, $context);
+
+        return $data;
+    }
+
+    public function supportsNormalization($data, string $format = null, array $context = [])
+    {
+        if (isset($context[self::ALREADY_CALLED])) {
+            return false;
+        }
+
+        return $data instanceof User;
+    }
+
+    public function hasCacheableSupportsMethod(): bool
+    {
+        return false;
+    }
+
+    private function userIsOwner(User $user): bool
+    {
+        return rand(0,10) > 5;
+    }
+}

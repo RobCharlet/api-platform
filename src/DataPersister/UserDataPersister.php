4c91fb93aef2bb71bf0f8267d7e74b68d71c1e35
Robin Charlet <robin.charlet@laposte.net>
2021-01-09 13:36:40 +0100

    Log User creation and update thanks to ContextAwareDataPersisterInterface

--- src/DataPersister/UserDataPersister.php
+++ src/DataPersister/UserDataPersister.php
@@ -4,23 +4,30 @@
 namespace App\DataPersister;
 
 
-use ApiPlatform\Core\DataPersister\DataPersisterInterface;
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
 use App\Entity\User;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class UserDataPersister implements DataPersisterInterface
+class UserDataPersister implements ContextAwareDataPersisterInterface
 {
 
     private $userPasswordEncoder;
     private $dataPersister;
+    private $logger;
 
-    public function __construct($dataPersister, UserPasswordEncoderInterface $userPasswordEncoder)
+    public function __construct(
+        $dataPersister,
+        UserPasswordEncoderInterface $userPasswordEncoder,
+        LoggerInterface $logger
+    )
     {
         $this->userPasswordEncoder = $userPasswordEncoder;
         $this->dataPersister = $dataPersister;
+        $this->logger = $logger;
     }
 
-    public function supports($data): bool
+    public function supports($data, array $context= []): bool
     {
         return $data instanceof User;
     }
@@ -28,8 +35,22 @@ class UserDataPersister implements DataPersisterInterface
     /**
      * @param User $data
      */
-    public function persist($data)
+    public function persist($data, array $context= [])
     {
+        if (($context['collection_operation_name'] ?? null) === 'put') {
+            $this->logger->info(sprintf('User %s is being updated', $data->getId()));
+        }
+
+        /**
+         * If object not created
+         */
+        if (!$data->getId()) {
+            // take any actions needed for a new user
+            // send registration email
+            // integrate into some CRM or payment system
+            $this->logger->info(sprintf('User %s just registered', $data->getEmail()));
+        }
+
         /**
          * Encode password before persist
          */
@@ -44,7 +65,7 @@ class UserDataPersister implements DataPersisterInterface
         $this->dataPersister->persist($data);
     }
 
-    public function remove($data)
+    public function remove($data, array $context= [])
     {
         $this->dataPersister->remove($data);
     }

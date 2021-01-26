9178a6794de3217967b9109526e380b3bc825b76
Robin Charlet <robin.charlet@laposte.net>
2021-01-26 13:26:45 +0100

    Create a custom search filter for CheeseListing

---
+++ src/ApiPlatform/CheeseSearchFilter.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\ApiPlatform;
+
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use Doctrine\ORM\QueryBuilder;
+
+class CheeseSearchFilter extends AbstractFilter
+{
+    protected function filterProperty(
+        string $property,
+        $value,
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        string $operationName = null
+    ) {
+        if ($property !== 'search') {
+            return;
+        }
+
+        $alias = $queryBuilder->getRootAliases()[0];
+        // a param nam that is guaranteed unique in this query
+        $valueParameter = $queryNameGenerator->generateParameterName('search');
+        $queryBuilder
+            ->andWhere(sprintf(
+                '%s.title LIKE :%s OR %s.description LIKE :%s',
+                $alias, $valueParameter, $alias, $valueParameter
+            ))
+            ->setParameter($valueParameter, '%'.$value.'%');
+    }
+
+    public function getDescription(string $resourceClass): array
+    {
+        return [
+            'search' => [
+                'property' => null,
+                'type' => 'string',
+                'required' => false,
+                'openapi' => [
+                    'description' => 'Search across multiple fields',
+                ]
+            ]
+        ];
+    }
+}
--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -8,6 +8,7 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
 use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
+use App\ApiPlatform\CheeseSearchFilter;
 use App\Repository\CheeseListingRepository;
 use App\Validator\IsValidOwner;
 use App\Validator\ValidIsPublished;
@@ -54,6 +55,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * })
  * @ApiFilter(RangeFilter::class, properties={"price"})
  * @ApiFilter(PropertyFilter::class)
+ * @ApiFilter(CheeseSearchFilter::class)
  *
  * @ORM\Entity(repositoryClass=CheeseListingRepository::class)
  * @ORM\EntityListeners({"App\Doctrine\CheeseListingSetOwnerListener"})
